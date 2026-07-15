2c737aafc6d70d1944cfd7fad81e615b7bad572e
edwin34776-maker <edwin34776@gmail.com>
2026-07-15 12:15:36 -0500

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -3,7 +3,7 @@ from supabase import create_client
 # Tu URL de Supabase
 SUPABASE_URL = "https://cpazmoebqbsrahviifvp.supabase.co"
 
-# Tu ANON KEY - LA ENCUENTRAS EN SUPABASE → Settings → API → "anon public"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwYXptb2VicWJzcmFodmlpZnZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE5ODI0MDAsImV4cCI6MjA1NzU1ODQwMH0."  # <-- REEMPLAZA CON TU ANON KEY REAL
+# Tu ANON KEY
+SUPABASE_KEY = "sb_publishable_UAJPuKcM42lrD0rXJ8c4AA_JNATpTc_"
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
