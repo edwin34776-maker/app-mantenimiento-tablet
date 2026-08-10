a4d6be93a4b1677be06d4237dad0612c5088ce47
edwin34776-maker <edwin34776@gmail.com>
2026-08-10 13:20:47 -0500

    Update app_mantenimiento_tablet.py

--- MANTENIMIENTO/app_mantenimiento_tablet.py
+++ MANTENIMIENTO/app_mantenimiento_tablet.py
@@ -812,7 +812,10 @@ def pantalla_login():
         <div class="perfil-card perfil-admin" style="text-align: center; padding: 20px;">
             <div class="perfil-icon">&#128100;</div>
             <div class="perfil-titulo" style="color: #dc3545;">ADMIN</div>
-            <div class="perfil-desc">Asigna tecnicos<br>Cambia prioridades<br>Verifica ejecuciones<br>Ve todo el sistema</div>
+            <div class="perfil-desc">
+                <div>Asigna tecnicos</div>
+                <div>Verifica ejecuciones</div>
+            </div>
         </div>
         """, unsafe_allow_html=True)
 
@@ -827,7 +830,10 @@ def pantalla_login():
         <div class="perfil-card perfil-tecnico" style="text-align: center; padding: 20px;">
             <div class="perfil-icon">&#128295;</div>
             <div class="perfil-titulo" style="color: #28a745;">TECNICO</div>
-            <div class="perfil-desc">Ve sus ordenes<br>Ejecuta actividades<br>Comenta y reporta</div>
+            <div class="perfil-desc">
+                <div>Ve sus ordenes</div>
+                <div>Ejecuta actividades</div>
+            </div>
         </div>
         """, unsafe_allow_html=True)
         if st.button("ENTRAR COMO TECNICO", use_container_width=True, type="primary", key=gen_key("login_tecnico")):
